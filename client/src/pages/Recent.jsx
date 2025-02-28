34cc2a7e7c42522e50a496065c92cf14c3572fd1
veereshk20 <pandiriveereshkumar@gmail.com>
2025-02-28 19:33:21 +0530

    added collabEdit, FileConvertor, Starred , Recent pages to the frontend

--- client/src/pages/Recent.jsx
+++ client/src/pages/Recent.jsx
@@ -1,8 +1,50 @@
 import React from 'react'
+import FileTable from '@/components/FilesTable'
+
+const files = [
+  {
+    id:1,
+    name: "Analysis.txt",
+    modifiedAt: "Sept 23, 2025",
+    owner: "me",
+    collaborators: ["KD", "GA", "GS"],
+    starred: false
+  },
+  {
+    id:2,
+    name: "Analysis.txt",
+    modifiedAt: "Oct 23, 2025",
+    owner: "Pandiri Veeresh Kumar",
+    collaborators: ["KD", "GA", "GS"],
+    starred: false
+  },
+  {
+    id:3,
+    name: "Analysis.txt",
+    modifiedAt: "Nov 23, 2025",
+    owner: "Pandiri Veeresh Kumar",
+    collaborators: ["KD", "GA", "GS"],
+    starred: true
+  },
+  {
+    id:4,
+    name: "Analysis.txt",
+    modifiedAt: "Dec 23, 2025",
+    owner: "Pandiri Veeresh Kumar",
+    collaborators: ["KD", "GA", "GS"],
+    starred: true
+  },
+];
+
+//from the backend itself we should get the files in sorted order
 
 const Recent = () => {
   return (
-    <div>Recent</div>
+    <main className="flex-1 p-6 overflow-y-auto m-10 rounded-2xl" style={{ backgroundColor: "#F2F6FE" }}>
+          <h1 className="text-3xl font-semibold mb-4">Recent</h1>
+          <hr className="h-[3px] my-5 border-0" style={{backgroundColor : "#95ADDC"}}/>
+          <FileTable files={files} isStarredPage={false}/>
+    </main>
   )
 }
 
