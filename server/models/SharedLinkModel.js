3ccfcaee0ca1324128664928041420cafd708e78
Thakursaimadan <148352466+Thakursaimadan@users.noreply.github.com>
2025-03-01 05:33:48 +0530

    sharedlink route successful (#10)

--- server/models/SharedLinkModel.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const SharedLinkSchema = new mongoose.Schema({
-    LinkId: String,
-    Permisson : Array
-
-})
-export const SharedLink = mongoose.models.SharedLink || mongoose.model('SharedLink', SharedLinkSchema);
